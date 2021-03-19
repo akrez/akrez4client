814455ac9c1a72c42995e9e4033762c767ed33ab
akrez <akrez.like@gmail.com>
2021-03-20 02:10:40 +0330

    Bootstrap4Asset

--- assets/cdn/Bootstrap4Asset.php
+++ assets/cdn/Bootstrap4Asset.php
@@ -1,9 +1,11 @@
 <?php
+
 /**
  * @link http://www.yiiframework.com/
  * @copyright Copyright (c) 2008 Yii Software LLC
  * @license http://www.yiiframework.com/license/
  */
+
 namespace app\assets\cdn;
 
 use yii\web\AssetBundle;
@@ -19,7 +21,10 @@ class Bootstrap4Asset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        'cdn/css/bootstrap4-rtl.min.css',
+        YII_ENV !== 'prod' ? 'cdn/css/bootstrap4-rtl.css' : 'cdn/css/bootstrap4-rtl.min.css',
+    ];
+    public $js = [
+        'cdn/js/bootstrap.bundle.min.js',
     ];
     public $depends = [
         'yii\bootstrap4\BootstrapAsset',
