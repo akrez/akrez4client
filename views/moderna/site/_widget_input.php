0fcebb2e35aee4f0a650bf63616b4eb5d62cd852
akrez <akrez.like@gmail.com>
2021-03-25 01:08:00 +0430

    widget input

--- views/moderna/site/_widget_input.php
+++ views/moderna/site/_widget_input.php
@@ -4,18 +4,22 @@ use app\models\Blog;
 use yii\helpers\Html;
 
 ?>
-<div class="input-group">
-    <span class="input-group-addon">
-        <?= Html::tag('label', $field['title'] . ' ' . Blog::getConstant('widget', $field['widget']), ['class' => 'control-label', 'style' => 'margin-top: 3px;margin-bottom: 2px;']) ?>
-    </span>
-    <?= Html::hiddenInput($namePrefix . '[operation]', $widget); ?>
-    <?= Html::textInput($namePrefix . '[value]', $filter['value'], ['class' => 'form-control',]); ?>
-    <?= (empty($field['unit']) ? '' : Html::tag('span', $field['unit'], ['class' => 'input-group-addon'])); ?>
-    <?php if ($filter['value'] !== null) : ?>
-        <span class="input-group-btn">
-            <button class="btn btn-danger btn-delete" type="button" style="height: 34px;padding-top: 9px;">
-                <span class="glyphicon glyphicon-trash" aria-hidden="true"></span>
-            </button>
-        </span>
+<div class="input-group flex-fill">
+    <div class="input-group-prepend">
+        <?= Html::tag('span', Html::encode($field['title']) . ' ' . Blog::getConstant('widget', $filter['widget']), ['class' => 'input-group-text']) ?>
+    </div>
+    <?= Html::hiddenInput($namePrefix . '[operation]', $filter['widget']); ?>
+    <?= Html::textInput($namePrefix . '[value]', $filter['value'], ['class' => 'form-control']); ?>
+    <?php if (mb_strlen($field['unit']) > 0) : ?>
+        <div class="input-group-append">
+            <?= Html::tag('span', Html::encode($field['unit']), ['class' => 'input-group-text']) ?>
+            <?php
+            /*
+            if ($filter['value'] !== null) :
+                Html::tag('span', '<i class="fa fa-trash" aria-hidden="true"></i>', ['class' => 'btn btn-danger btn-delete']);
+            endif 
+            */
+            ?>
+        </div>
     <?php endif ?>
 </div>
