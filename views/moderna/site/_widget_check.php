552ec3ebc42372bc752b1fa6dede254c331e5c52
akrez <akrez.like@gmail.com>
2021-03-31 21:05:47 +0430

    check

--- views/moderna/site/_widget_check.php
+++ views/moderna/site/_widget_check.php
@@ -6,20 +6,22 @@ use yii\helpers\HtmlPurifier;
 if ($isSingle) {
     $operation = '=';
     $input = Html::radioList($namePrefix . '[value]', $filter['value'], array_combine($items, $items), ['separator' => "<br />", 'encode' => false]);
+    $noneBtnType = 'radio';
 } else {
     $operation = 'IN';
     $input = Html::checkboxList($namePrefix . '[values][]', $filter['values'], array_combine($items, $items), ['separator' => "<br />", 'encode' => false]);
+    $noneBtnType = 'checkbox';
 }
 
 ?>
-<div class="card mb-2 card-akrez">
-    <div class="card-header">
-        <?= Html::checkbox($namePrefix . '[operation]', !$disabled, ['value' => $operation]); ?>
+<div class="card card-akrez">
+    <div class="card-header p-2">
+        <?= Html::checkbox($namePrefix . '[operation]', !$disabled, ['value' => $operation, 'class' => 'ml-1']); ?>
         <?= HtmlPurifier::process($field['title']) ?>
         <?= $field['unit'] ? ' <small>(' . HtmlPurifier::process($field['unit']) . ')</small> ' : '' ?>
     </div>
     <div class="card-body p-2 collapse <?= $disabled ? '' : 'show' ?>">
         <?= $input ?>
-        <a onclick="$(this).closest('.filter').find('input[type=radio]').prop('checked', false)" href="javascript:void(0);" style="padding-right: 15px;"><small class="control-label">(هیچکدام)</small></a>
+        <a onclick="$(this).closest('.card-body').find('input[type=<?= $noneBtnType ?>]').prop('checked', false)" href="javascript:void(0);" style="padding-right: 15px;"><small class="control-label">(هیچکدام)</small></a>
     </div>
 </div>
