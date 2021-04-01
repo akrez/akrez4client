207f0c8d5bab5506e187e6ac9fdd64f2655c0536
akrez <akrez.like@gmail.com>
2021-04-02 02:08:55 +0430

    between

--- views/moderna/site/_widget_between.php
+++ views/moderna/site/_widget_between.php
@@ -1,56 +1,82 @@
 <?php
 
+use app\models\Blog;
 use kartik\slider\Slider;
 use yii\helpers\Html;
 use yii\helpers\HtmlPurifier;
 use yii\web\JsExpression;
 
-$idPrefix = 'Search-' . '-' . $i;
-echo Html::hiddenInput($namePrefix . '[operation]', $widget);
+$idPrefix = 'Search-' . $i;
+
+$min = 0;       //default value
+$max = 100;     //default value
+
+$items = array_keys($items);
+$items = array_map('floatval', $items);
+$items = Blog::filterArray($items);
+if (count($items) > 0) {
+    $min = min($items);
+    $max = max($items);
+}
+
+if ($max == $min) {
+    $max = $min + 1;
+}
+
+if (!strlen($filter['value_min'])) {
+    $filter['value_min'] = $min;
+}
+if (!strlen($filter['value_max'])) {
+    $filter['value_max'] = $max;
+}
+//
+$min = floatval($min);
+$max = floatval($max);
+$filter['value_min'] = floatval($filter['value_min']);
+$filter['value_max'] = floatval($filter['value_max']);
+
+
 ?>
 
-<div class="col-sm-12 filter">
-    <div class="panel panel-default panel-akrez">
-        <div class="panel-heading <?= $disabled ? 'panel-collapsed' : '' ?>" data-idPrefix="<?= $idPrefix ?>">
-            <?= Html::checkbox(null, !$disabled); ?>
-            <?= HtmlPurifier::process($field['title']) ?>
-            <?= $field['unit'] ? ' <small>(' . HtmlPurifier::process($field['unit']) . ')</small> ' : '' ?>
-        </div>
-        <div class="panel-body" style="<?= $disabled ? 'display: none;' : '' ?>">
+<div class="card card-akrez">
+    <div class="card-header p-2">
+        <?= Html::checkbox($namePrefix . '[operation]', !$disabled, ['value' => 'BETWEEN', 'class' => 'ml-1']); ?>
+        <?= HtmlPurifier::process($field['title']) ?>
+        <?= $field['unit'] ? ' <small>(' . HtmlPurifier::process($field['unit']) . ')</small> ' : '' ?>
+    </div>
+    <div class="card-body p-2 collapse <?= $disabled ? '' : 'show' ?>">
+
+        <?php
+        echo "<div><span style='float: left;' id='$idPrefix-label-min'>" . number_format($filter['value_min']) . "</span><span style='float: right;' id='$idPrefix-label-max'>" . number_format($filter['value_max']) . "</span><div class='clearfix'></div></div>";
+        echo "<div class='text-center'>";
+        echo Slider::widget([
+            'options' => [
+                'id' => $idPrefix . '-slider',
+            ],
+            'name' => $namePrefix . "[input]",
+            'value' => implode(',', [$filter['value_min'], $filter['value_max']]),
+            'sliderColor' => Slider::TYPE_GREY,
+            'pluginOptions' => [
+                'min' => $min,
+                'max' => $max,
+                'range' => true,
+                'tooltip' => 'hide',
+            ],
+            'pluginEvents' => [
+                'slide' => new JsExpression("function( event ) { " .
+                    "$('#{$idPrefix}-label-min').text(event.value[0].toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')); " .
+                    "$('#{$idPrefix}-label-max').text(event.value[1].toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')); " .
+                    "$('#{$idPrefix}-input-min').val(event.value[0]); " .
+                    "$('#{$idPrefix}-input-max').val(event.value[1]); " .
+                    "}"),
+            ],
+        ]);
+        echo "</div>";
+
+        echo Html::hiddenInput($namePrefix . '[value_min]', $filter['value_min'], ['id' => $idPrefix . '-input-min']);
+        echo Html::hiddenInput($namePrefix . '[value_max]', $filter['value_max'], ['id' => $idPrefix . '-input-max']);
+        ?>
 
 
-
-            <?php
-            echo "<div><span style='float: left;' id='$idPrefix-label-min'>" . number_format($filter['value_min']) . "</span><span style='float: right;' id='$idPrefix-label-max'>" . number_format($filter['value_max']) . "</span><div class='clearfix'></div></div>";
-            echo Slider::widget([
-                'options' => [
-                    'id' => $idPrefix . '-slider',
-                ],
-                'name' => $namePrefix . "[input]",
-                'value' => implode(',', [$filter['value_min'], $filter['value_max']]),
-                'sliderColor' => Slider::TYPE_GREY,
-                'pluginOptions' => [
-                    'min' => $min,
-                    'max' => $max,
-                    'range' => true,
-                    'tooltip' => 'hide',
-                ],
-                'pluginEvents' => [
-                    'slide' => new JsExpression("function( event ) { " .
-                        "$('#{$idPrefix}-label-min').text(event.value[0].toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')); " .
-                        "$('#{$idPrefix}-label-max').text(event.value[1].toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')); " .
-                        "$('#{$idPrefix}-input-min').val(event.value[0]); " .
-                        "$('#{$idPrefix}-input-max').val(event.value[1]); " .
-                        "}"),
-                ],
-            ]);
-
-            echo Html::hiddenInput($namePrefix . '[value_min]', $filter['value_min'], ['id' => $idPrefix . '-input-min'] + ($disabled ? ['disabled' => 'disabled'] : []));
-            echo Html::hiddenInput($namePrefix . '[value_max]', $filter['value_max'], ['id' => $idPrefix . '-input-max'] + ($disabled ? ['disabled' => 'disabled'] : []));
-            ?>
-
-
-
-        </div>
     </div>
 </div>
