dd744ea4008f459bd5d36b8841c45c9a9749217c
AliAkbar <akrez@akrez.com>
2021-05-03 00:28:17 +0430

    app translate

---
+++ messages/en-US/app.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'Signin' => 'Signin',
+    'Signout' => 'Signout',
+    'Signup' => 'Signup',
+    'Title' => 'Title',
+    'Price' => 'Price',
+    'Search' => 'Search',
+    'Basket' => 'Basket',
+    'Invoice' => 'Invoice',
+    'Categories' => 'Categories',
+    'Reset Password Request' => 'Reset Password Request',
+];
