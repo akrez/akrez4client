01cedf3fb1daa70c14f60daf1e68b6c510aa4064
AliAkbar <akrez@akrez.com>
2021-06-24 00:49:32 +0430

    equal row

--- views/moderna/site/_products_container.php
+++ views/moderna/site/_products_container.php
@@ -22,51 +22,24 @@ $this->registerCss("
 .fs10 {
     font-size: 1.0rem;
 }
-.card {
+.row.equal {
+    display: flex;
+    flex-wrap: wrap;
+}
+.thumbnail {
     color: #000000;
 }
-a.card {
+a.thumbnail {
     text-decoration: none;
 }
-
-.card img {
+.thumbnail img {
     text-decoration: none;
 }
-
-.card .caption * {
+.thumbnail .caption * {
     margin: 9px 0 0;
 }
-
-.card .caption h5 {
+.thumbnail .caption h5 {
     font-weight: bold;
-}
-@media (min-width: 576px) {
-    .card-columns {
-        -webkit-column-count: 1;
-        -moz-column-count: 1;
-        column-count: 1;
-    }
-}
-@media (min-width: 768px) {
-    .card-columns {
-        -webkit-column-count: 2;
-        -moz-column-count: 2;
-        column-count: 2;
-    }
-}
-@media (min-width: 992px) {
-    .card-columns {
-        -webkit-column-count: 3;
-        -moz-column-count: 3;
-        column-count: 3;
-    }
-}
-@media (min-width: 1200px) {
-    .card-columns {
-        -webkit-column-count: 4;
-        -moz-column-count: 4;
-        column-count: 4;
-    }
 }
 ");
 ?>
@@ -87,62 +60,61 @@ a.card {
         </div>
     </div>
 
-    <div class="card-columns">
-        <?php
-        foreach (Blog::getData('products') as $product) :
-            $title = HtmlPurifier::process($product['title']);
-        ?>
-            <a class="card" href="<?= Blog::url('site/product', ['id' => $product['id']]) ?>">
-                <?php
-                if ($product['image']) :
-                    echo Html::img(Blog::getImage('product', '400', $product['image']), ['class' => 'img-fluid rounded', 'alt' => $title]);
-                endif;
-                ?>
-                <div class="card-body p-2">
-                    <h5 class="card-title fs10"><?= $title ?></h5>
-                    <p class="card-text fs09">
-                        <?php
-                        foreach ($product['_fields'] as $field) :
-                            if ($field['in_summary'] !== "0") :
-                                echo ' <strong> ' . HtmlPurifier::process($field['field']) . ': ' . '</strong> ';
-                                echo HtmlPurifier::process(implode(' ,', $field['values']) . ' ' . $field['unit']);
-                                echo '<br>';
-                            endif;
-                        endforeach;
-                        ?>
-                    </p>
-
+    <div class="container-fluid">
+        <div class="row equal">
+            <?php
+            foreach (Blog::getData('products') as $product) :
+                $title = HtmlPurifier::process($product['title']);
+            ?>
+                <a class="thumbnail border pt-3 col-xs-12 col-sm-6 col-md-4 col-lg-3" href="<?= Blog::url('site/product', ['id' => $product['id']]) ?>">
                     <?php
-                    if (empty($product['price_min']) && empty($product['price_max'])) :
-                    else :
-                        echo '<p class="text-left m-0">';
-                        if (!empty($product['price_min']) && !empty($product['price_max'])) :
-                            if ($product['price_min'] == $product['price_max']) :
-                                echo Yii::$app->formatter->asPrice($product['price_min']);
-                            else :
-                                echo ' از ' . Yii::$app->formatter->asPrice($product['price_min']) . '<br>' . ' تا ' . Yii::$app->formatter->asPrice($product['price_max']);
-                            endif;
-                        else :
-                            if (!empty($product['price_min'])) :
-                                echo ' از ' . Yii::$app->formatter->asPrice($product['price_min']);
-                            endif;
-                            if (!empty($product['price_max'])) :
-                                echo ' تا ' . Yii::$app->formatter->asPrice($product['price_max']);
-                            endif;
-                        endif;
-                        echo '</p>';
+                    if ($product['image']) :
+                        echo Html::img(Blog::getImage('product', '400', $product['image']), ['class' => 'img-fluid rounded', 'alt' => $title]);
                     endif;
-
                     ?>
-                </div>
-            </a>
-        <?php endforeach ?>
+                    <div class="card-body p-2">
+                        <h5 class="card-title fs10"><?= $title ?></h5>
+                        <p class="card-text fs09">
+                            <?php
+                            foreach ($product['_fields'] as $field) :
+                                if ($field['in_summary'] !== "0") :
+                                    echo ' <strong> ' . HtmlPurifier::process($field['field']) . ': ' . '</strong> ';
+                                    echo HtmlPurifier::process(implode(' ,', $field['values']) . ' ' . $field['unit']);
+                                    echo '<br>';
+                                endif;
+                            endforeach;
+                            ?>
+                        </p>
+
+                        <?php
+                        if (empty($product['price_min']) && empty($product['price_max'])) :
+                        else :
+                            echo '<p class="text-left m-0">';
+                            if (!empty($product['price_min']) && !empty($product['price_max'])) :
+                                if ($product['price_min'] == $product['price_max']) :
+                                    echo Yii::$app->formatter->asPrice($product['price_min']);
+                                else :
+                                    echo ' از ' . Yii::$app->formatter->asPrice($product['price_min']) . '<br>' . ' تا ' . Yii::$app->formatter->asPrice($product['price_max']);
+                                endif;
+                            else :
+                                if (!empty($product['price_min'])) :
+                                    echo ' از ' . Yii::$app->formatter->asPrice($product['price_min']);
+                                endif;
+                                if (!empty($product['price_max'])) :
+                                    echo ' تا ' . Yii::$app->formatter->asPrice($product['price_max']);
+                                endif;
+                            endif;
+                            echo '</p>';
+                        endif;
+
+                        ?>
+                    </div>
+                </a>
+            <?php endforeach ?>
+        </div>
     </div>
 
-
-
-
-    <div class="row">
+    <div class="row mt-3">
         <div class="col-sm-12">
             <?= LinkPager::widget(['pagination' => $pagination, 'hideOnSinglePage' => false]); ?>
         </div>
